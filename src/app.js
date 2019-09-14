b153eda3bb0118a448c206401d2f1864ac5fc0be
Udayaditya Singh <udayadityasingh@Udayadityas-MacBook-Air.local>
2019-09-14 21:12:50 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //making paths for setting
 const publicDirPath = path.join(__dirname, '../public')
@@ -84,6 +85,6 @@ app.get('/*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server in up!')
 })
